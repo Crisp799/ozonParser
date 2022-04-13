6807e03975e108f8b62c45255d8123396a484267
Crisp799 <mr.nick2001@mail.ru>
2022-04-13 16:39:40 +0300

    Добавлен SKU парсер

--- src/Controller/ParserServiceController.php
+++ src/Controller/ParserServiceController.php
@@ -28,11 +28,20 @@ class ParserServiceController extends AbstractController
         //echo $crawler->html();
         $goodsArray =[];
         $converter = new CssSelectorConverter();
-        $product = $crawler->filter('.n4i');
-        $links = $crawler->filter('.im7')->extract(['attr' => 'href']);
+        //$product = $crawler->filter('.n4i');
+        //$product = $crawler->filter('.widget-search-result-container >div > div');
+        $product = $crawler->filterXPath('//*[@id="layoutPage"]/div[1]/div[3]/div[2]/div[2]/div[3]/div[1]/div/div/div');
+        //echo $product->count();
+        //$links = $crawler->filter('.widget-search-result-container >div > div > .im7')->extract(['attr' => 'href']); // сделать зависимым не от тега
+        $links = $crawler->filterXPath('//*[@id="layoutPage"]/div[1]/div[3]/div[2]/div[2]/div[3]/div[1]/div/div/div/a')->extract(['attr' => 'href']); // сделать зависимым не от тега
+
+        var_dump($links);
         $prices = $crawler->filter('.ui-t');
-        foreach ($product as $domElement) {
-            $this->getCountOfReview($domElement->textContent).' ';
+        foreach ($links as $link) {
+            echo $this->getSKU($link).' ';
+        }
+        foreach ($product as $key => $domElement) {
+            //echo $this->getProductName($domElement->textContent).' ';
             //echo $domElement->textContent.'____';
             //array_push($goodsArray, $domElement->textContent);
             //var_dump($domElement);
@@ -64,17 +73,42 @@ class ParserServiceController extends AbstractController
         return intval($price);
     }
 
-    public function getCountOfReview($string) :int
+    public function getCountOfReview($string) :?int
     {
         $finishIndex = mb_strpos($string, 'отз')-2;
-        echo $string=mb_substr($string,0,$finishIndex+1);
+        $string=mb_substr($string,0,$finishIndex+1);
         $matches = [];
-        preg_match('/\d+$/',$string,$matches);
-        var_dump($matches);
+        if(preg_match('/\d+$/',$string,$matches) === 0)
+            return null;
+        //var_dump($matches);
         return intval($matches[0]);
     }
 
-    public function getProductName() {
+    public function getProductName($string) :?string
+    {
+        $countOfReview = $this->getCountOfReview($string);
 
+        $string = str_replace('Бестселлер','', $string); //Убираем слово бестселлер
+        $finishLineIndex = mb_strpos($string, 'отз') - 2;
+        $string = mb_substr($string, 0, $finishLineIndex + 1);
+
+        $startIndex = mb_strpos($string, '₽'); //обрезаем строку оставляя только название и количество отзывов
+        $string = mb_substr($string, $startIndex + 1);
+        $startIndex = mb_strpos($string, '₽');
+        $string = mb_substr($string, $startIndex + 1);
+
+        $finishLineIndex = mb_strpos($string,strval($countOfReview));
+        $string = mb_substr($string, 0, $finishLineIndex -1);
+        //echo $startIndex;
+        return $string;
+
+    }
+
+    public function getSKU($link) :?int
+    {
+        $matches =[];
+        if(preg_match('/\d{5,}/', $link, $matches) === false)
+            return null;
+        return intval($matches[0]);
     }
 }
